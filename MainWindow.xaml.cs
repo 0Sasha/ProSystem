f914672657a1cfe2afe905c7fc5477c50da80853
ADMIN <ADMIN@DESKTOP-IVITJ1V>
2023-07-17 11:58:25 +0600

    New

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -238,7 +238,8 @@ public partial class MainWindow : Window
             NewTool.Show();
         }
     }
-    private void UpdateToolbarContext(object sender, RoutedEventArgs e) => ToolsView.Items.Refresh();
+    private void UpdateToolbarContext(object sender, RoutedEventArgs e) =>
+        ToolsView.Items.Refresh();
     private void ReloadBarsToolContext(object sender, RoutedEventArgs e)
     {
         if (ToolsView.SelectedItem != null)
